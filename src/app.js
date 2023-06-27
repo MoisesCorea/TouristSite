a25da02d57f7ae6314763189ee9d3b05412f2dc5
MoisesCorea <mcoreav@uoc.edu>
2023-06-27 09:51:15 -0600

    Cambios previos a la prueba

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
+import 'lazysizes';
+// import a plugin
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const button = document.getElementById('buttonPortada')
   button.addEventListener('click', () => {
     window.location.href = 'sitios.html'
   })
 })
+
