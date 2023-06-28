e1025fdccf1fa5c16b695a0725f4d2338b934e1a
MoisesCorea <mcoreav@uoc.edu>
2023-06-28 15:18:29 -0600

    Cambios primer informe 3

--- src/app.js
+++ src/app.js
@@ -10,3 +10,14 @@ const button = document.getElementById('buttonPortada');
 button.addEventListener('click', () => {
   window.location.href = 'sitios.html';
 });
+
+document.addEventListener('touchstart', onTouchStart, {passive: true});
+
+const observer = new PerformanceObserver((list) => {
+  for (const entry of list.getEntries()) {
+    // Attribution entry including "containerSrc":"https://example.com"
+    console.log(JSON.stringify(entry.attribution));
+  }
+});
+
+observer.observe({entryTypes: ['longtask']});
